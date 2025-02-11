357f091984600c90eb46c46c81e4898375b06321
NotNoelChannel <52098241+NotNoelChannel@users.noreply.github.com>
2025-02-11 20:00:21 +0100

    Update kernel.mk

--- board/kernel.mk
+++ board/kernel.mk
@@ -1,7 +1,7 @@
 # Kernel informations
 BOARD_KERNEL_BASE := 0x00200000
 BOARD_KERNEL_PAGESIZE := 4096
-BOARD_KERNEL_CMDLINE := androidboot.hardware=$(BOARD_VENDOR_PLATFORM) androidboot.selinux=permissive
+BOARD_KERNEL_CMDLINE := androidboot.hardware=e0 androidboot.selinux=permissive
 BOARD_MKBOOTIMG_ARGS := --ramdisk_offset 0x1200000
 
 # Kernel properties
