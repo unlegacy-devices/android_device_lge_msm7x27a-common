6361c26917ec54bc48c727596cdb2f826bce7f48
NotNoelChannel <52098241+NotNoelChannel@users.noreply.github.com>
2025-02-11 20:22:42 +0100

    Update platform.mk

--- product/platform.mk
+++ product/platform.mk
@@ -4,5 +4,5 @@ PRODUCT_COPY_FILES += \
 
 # Platform device configuration
 PRODUCT_COPY_FILES += \
-    device/lge/$(LGE_MSM7X27A_DEVICE)/rootdir/root/fstab.$(BOARD_VENDOR_PLATFORM):root/fstab.$(BOARD_VENDOR_PLATFORM) \
-    device/lge/$(LGE_MSM7X27A_DEVICE)/rootdir/root/ueventd.$(BOARD_VENDOR_PLATFORM).rc:root/ueventd.$(BOARD_VENDOR_PLATFORM).rc
+    device/lge/e400/rootdir/root/fstab.$(BOARD_VENDOR_PLATFORM):root/fstab.$(BOARD_VENDOR_PLATFORM) \
+    device/lge/e400/rootdir/root/ueventd.$(BOARD_VENDOR_PLATFORM).rc:root/ueventd.$(BOARD_VENDOR_PLATFORM).rc
