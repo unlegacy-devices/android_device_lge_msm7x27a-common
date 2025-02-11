2fb1b215d5d3c8c64fa18f925ae688a429e5c873
NotNoelChannel <52098241+NotNoelChannel@users.noreply.github.com>
2025-02-11 20:22:18 +0100

    Update input.mk

--- product/input.mk
+++ product/input.mk
@@ -2,7 +2,3 @@
 PRODUCT_COPY_FILES += \
     device/lge/msm7x27a-common/rootdir/system/usr/keylayout/7k_handset.kl:system/usr/keylayout/7k_handset.kl \
     device/lge/msm7x27a-common/rootdir/system/usr/keylayout/7x27a_kp.kl:system/usr/keylayout/7x27a_kp.kl
-
-# Device Keys layouts
-PRODUCT_COPY_FILES += \
-    device/lge/$(LGE_MSM7X27A_DEVICE)/rootdir/system/usr/keylayout/$(BOARD_VENDOR_PLATFORM)_keypad.kl:system/usr/keylayout/$(BOARD_VENDOR_PLATFORM)_keypad.kl
